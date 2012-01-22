86540a113a4d304a1b8ed0af6149ea938e486e7a
Sven Gothel <sgothel@jausoft.com>
2012-01-22 23:42:55 +0100

    Fix Bug 516 (Determine Java Version) / See gluegen: 64639b805a32338385421f168e12c1ef7f749d00

--- scripts/make.jogl.all.linux-x86_64.sh
+++ scripts/make.jogl.all.linux-x86_64.sh
@@ -4,6 +4,13 @@ if [ -e ../setenv-build-jogl-x86_64.sh ] ; then
     . ../setenv-build-jogl-x86_64.sh
 fi
 
+#    -Dtarget.sourcelevel=1.6 \
+#    -Dtarget.targetlevel=1.6 \
+#    -Dtarget.rt.jar=/opt-share/jre1.6.0_30/lib/rt.jar \
+
 ant  \
+    -Dtarget.sourcelevel=1.6 \
+    -Dtarget.targetlevel=1.6 \
+    -Dtarget.rt.jar=/opt-share/jre1.6.0_30/lib/rt.jar \
     -Drootrel.build=build-x86_64 \
     $* 2>&1 | tee make.jogl.all.linux-x86_64.log
